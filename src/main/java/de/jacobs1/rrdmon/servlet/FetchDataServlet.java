cda5d34ea6ef751af8dd384fd47f3343f11de027
Henning Jacobs <henning@jacobs1.de>
2012-09-14 17:35:10 +0200

    use 2sec memory cache to improve performance for concurrent requests

--- src/main/java/de/jacobs1/rrdmon/servlet/FetchDataServlet.java
+++ src/main/java/de/jacobs1/rrdmon/servlet/FetchDataServlet.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import java.util.Locale;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -16,6 +18,11 @@ import org.rrd4j.core.FetchData;
 import org.rrd4j.core.FetchRequest;
 import org.rrd4j.core.RrdDb;
 
+import com.google.common.base.Throwables;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
 import de.jacobs1.rrdmon.config.ApplicationConfig;
 import de.jacobs1.rrdmon.config.DataSource;
 
@@ -23,6 +30,97 @@ import de.jacobs1.rrdmon.config.DataSource;
  * @author  henning
  */
 public class FetchDataServlet extends HttpServlet {
+
+    private LoadingCache<String, String> dataCache;
+
+    public FetchDataServlet() {
+        dataCache = CacheBuilder.newBuilder().maximumSize(10000).concurrencyLevel(16)
+                                .expireAfterWrite(2, TimeUnit.SECONDS).build(new CacheLoader<String, String>() {
+                                        @Override
+                                        public String load(final String key) throws Exception {
+                                            return getDataJson(key);
+                                        }
+                                    });
+    }
+
+    private String getDataJson(final String timeSpec) throws IOException {
+
+        final ApplicationConfig config = ApplicationConfig.getInstance();
+
+        final StringBuilder out = new StringBuilder();
+
+        // we use a 2sec delay to make sure all data is already collected for our timespan
+        final long now = (System.currentTimeMillis() / 1000) - 2;
+
+        // 10m, 1h, 1d, 1w
+        final long startTime = now + GraphImageServlet.parseTime(timeSpec);
+
+        out.append('{');
+
+        boolean firstVal = true;
+
+        DataSource dataSource;
+        double val;
+        int i;
+
+        String doubleStr;
+        for (final String dsName : config.getDataSourceNames()) {
+
+            dataSource = config.getDataSource(dsName);
+
+            if (!firstVal) {
+                out.append(',');
+            }
+
+            out.append('"');
+            out.append(dsName);
+            out.append("\":[");
+
+            i = 0;
+            for (DataSource.RrdDataSourceEntry entry : dataSource.getRrdDataSources()) {
+                val = getRrdValue(entry.getRrdPath(), entry.getDsName(), startTime, now);
+                if (i > 0) {
+                    out.append(',');
+                }
+
+                if (Double.isNaN(val) || Double.isInfinite(val)) {
+                    out.append("null");
+                } else {
+                    doubleStr = String.format("%.6f", val);
+                    out.append(doubleStr);
+                }
+
+                i++;
+            }
+
+            out.append(']');
+            firstVal = false;
+
+        }
+
+        out.append('}');
+
+        return out.toString();
+    }
+
+    private Double getRrdValue(final String rrdPath, final String dsName, final long startTime, final long now)
+        throws IOException {
+
+        RrdDb rrd;
+        FetchRequest fetchRequest;
+        FetchData data;
+        double val;
+        rrd = new RrdDb(rrdPath, true);
+        try {
+            fetchRequest = rrd.createFetchRequest(ConsolFun.AVERAGE, startTime, now);
+            data = fetchRequest.fetchData();
+            val = data.getAggregate(dsName, ConsolFun.AVERAGE);
+        } finally {
+            rrd.close();
+        }
+
+        return val;
+    }
 
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -36,25 +134,17 @@ public class FetchDataServlet extends HttpServlet {
     @Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException,
         IOException {
-
-        final ApplicationConfig config = ApplicationConfig.getInstance();
-
         Locale.setDefault(Locale.US);
 
         final String[] path = request.getRequestURI().split("/");
-
-        // we use a 2sec delay to make sure all data is already collected for our timespan
-        final long now = (System.currentTimeMillis() / 1000) - 2;
-        long startTime = now - 60; // one minute ago
+        String timeSpec = "1m";
 
         for (int i = 1; i < path.length; i++) {
             final String pathElement = path[path.length - 1 - i];
             switch (i) {
 
                 case 1 :
-
-                    // 10m, 1h, 1d, 1w
-                    startTime = now + GraphImageServlet.parseTime(pathElement);
+                    timeSpec = pathElement;
                     break;
             }
         }
@@ -62,63 +152,11 @@ public class FetchDataServlet extends HttpServlet {
         response.setContentType("application/json");
 
         final PrintWriter out = response.getWriter();
-
-        out.write('{');
-
-        boolean firstVal = true;
-
-        DataSource dataSource;
-        RrdDb rrd;
-        FetchRequest fetchRequest;
-        FetchData data;
-        double val;
-        int i;
-
-        String doubleStr;
-        int decimalSep;
-        for (final String dsName : config.getDataSourceNames()) {
-
-            dataSource = config.getDataSource(dsName);
-
-            if (!firstVal) {
-                out.write(',');
-
-            }
-
-            out.write('"');
-            out.write(dsName);
-            out.write("\":[");
-
-            i = 0;
-            for (DataSource.RrdDataSourceEntry entry : dataSource.getRrdDataSources()) {
-
-                rrd = new RrdDb(entry.getRrdPath(), true);
-                try {
-                    fetchRequest = rrd.createFetchRequest(ConsolFun.AVERAGE, startTime, now);
-                    data = fetchRequest.fetchData();
-                    val = data.getAggregate(entry.getDsName(), ConsolFun.AVERAGE);
-                } finally {
-                    rrd.close();
-                }
-
-                if (i > 0) {
-                    out.write(',');
-                }
-
-                if (Double.isNaN(val) || Double.isInfinite(val)) {
-                    out.write("null");
-                } else {
-                    doubleStr = String.format("%.6f", val);
-                    out.write(doubleStr);
-                }
-
-                i++;
-            }
-
-            out.write(']');
-            firstVal = false;
+        try {
+            out.write(dataCache.get(timeSpec));
+        } catch (ExecutionException ex) {
+            throw Throwables.propagate(ex);
         }
 
-        out.write('}');
     }
 }
